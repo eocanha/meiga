84f8622196341b3d56fc5c80947cd5cdc3b6cbfc
Enrique Ocaña González <eocanha@igalia.com>
2009-06-04 19:48:37 +0200

    Check for Vala dependency at configure level
    And added build dependency at package level, but disabled
    dependency checking in ./build-package.sh because people
    having old distributions won't have the Vala package and will
    need to install it by hand.

--- build-package.sh
+++ build-package.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
-set -x
-dpkg-buildpackage -rfakeroot -b -uc
 
+CHECKDEPS=`dpkg-checkbuilddeps 2>&1`
+
+echo "--------------------------------------------------"
+echo "$CHECKDEPS"
+echo "--------------------------------------------------"
+echo
+echo
+
+if test -n "$CHECKDEPS"
+then
+  echo "Some build dependencies unmet, but building anyway..."
+  echo "Do you have an older distribution without Vala package?"
+  echo
+  echo
+  sleep 5;
+fi
+
+# If you're using a distribution for which an updated Vala package 
+# exists and you want to check the dependency, remove the "-d" option
+
+dpkg-buildpackage -d -rfakeroot -b -uc
